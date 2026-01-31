250ebe5addac2b6c657f48a62ca36f5df0d4d43c
itsnitesh16 <niteshkumarghn1@gmail.com>
2026-01-31 21:26:06 +0530

    tabs icons added

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,28 @@
+import { Ionicons } from "@expo/vector-icons"
 import { Tabs } from "expo-router"
 import React from 'react'
 
 export default function TabsLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="feed" />
-      <Tabs.Screen name="books" />
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color, size }) => <Ionicons name="home" size={size} color={color} />
+        }}
+      />
+      <Tabs.Screen
+        name="feed"
+        options={{
+          tabBarIcon: ({ color, size }) => <Ionicons name="play" size={size} color={color} />
+        }}
+      />
+      <Tabs.Screen
+        name="books"
+        options={{
+          tabBarIcon: ({ color, size }) => <Ionicons name="book" size={size} color={color} />
+        }}
+      />
     </Tabs>
   )
 }
